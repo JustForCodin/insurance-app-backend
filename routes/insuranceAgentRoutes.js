661b0c544c67af0c6c8b99f209889c6e2645f72a
JustForCodin <musicjohnliofficial@gmail.com>
2025-02-17 16:49:11 +0200

    Core server logic

---
+++ routes/insuranceAgentRoutes.js
@@ -0,0 +1,18 @@
+// routes/insuranceAgentRoutes.js
+import { Router } from 'express';
+const router = Router();
+import { 
+    getAllInsuranceAgents, 
+    getInsuranceAgentById, 
+    createInsuranceAgent, 
+    updateInsuranceAgent, 
+    deleteInsuranceAgent 
+} from '../controllers/InsuranceAgentController.js';
+
+router.get('/', getAllInsuranceAgents);
+router.get('/:id', getInsuranceAgentById);
+router.post('/', createInsuranceAgent);
+router.put('/:id', updateInsuranceAgent);
+router.delete('/:id', deleteInsuranceAgent);
+
+export default router;
